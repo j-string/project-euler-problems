e26b307aa4900786be97cc1abaee12dd984ecbcf
j-string <justin.michael.stringer@gmail.com>
2016-09-15 17:43:21 -0500

    simplified

--- p1-sum-of-multiples-below-x.rkt
+++ p1-sum-of-multiples-below-x.rkt
@@ -3,9 +3,8 @@
 (define (multiples_below_x x)
   (let ([y (- x 1)])
     (cond
-      [(= 0 y) 0]
-      [(= 0 (modulo y 5)) (+ y (multiples_below_x y))]
-      [(= 0 (modulo y 3)) (+ y (multiples_below_x y))]
+      [(= y 0) 0]
+      [(or (= 0 (modulo y 3)) (= 0 (modulo y 5))) (+ y (multiples_below_x y))]
       [else (multiples_below_x y)]
       )
     )
