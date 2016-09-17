502c4af964d2b89e30f727afb04da01acb41f234
j-string <justin.michael.stringer@gmail.com>
2016-09-17 15:48:52 -0500

    added tests and created helper function

--- p3-largest-prime-factor.rkt
+++ p3-largest-prime-factor.rkt
@@ -3,35 +3,58 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname p3-largest-prime-factor) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp") (lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp") (lib "draw.rkt" "teachpack" "htdp")) #f)))
 ;(check-expect (largest-prime-factor 10) 5)
 
-(check-expect (is-prime? 2 2) true)
-(check-expect (is-prime? 2 3) true)
-(check-expect (is-prime? 2 5) true)
-(check-expect (is-prime? 2 11) true)
-(check-expect (is-prime? 2 13) true)
-(check-expect (is-prime? 2 17) true)
-(check-expect (is-prime? 2 19) true)
-(check-expect (is-prime? 2 23) true)
+(check-expect (is-prime? 2) true)
+(check-expect (is-prime? 3) true)
+(check-expect (is-prime? 5) true)
+(check-expect (is-prime? 11) true)
+(check-expect (is-prime? 13) true)
+(check-expect (is-prime? 17) true)
+(check-expect (is-prime? 19) true)
+(check-expect (is-prime? 23) true)
 
-(check-expect (is-prime? 2 0) false)
-(check-expect (is-prime? 2 1) false)
-(check-expect (is-prime? 2 4) false)
-(check-expect (is-prime? 2 6) false)
-(check-expect (is-prime? 2 8) false)
-(check-expect (is-prime? 2 9) false)
-(check-expect (is-prime? 2 10) false)
+(check-expect (is-prime? 0) false)
+(check-expect (is-prime? 1) false)
+(check-expect (is-prime? 4) false)
+(check-expect (is-prime? 6) false)
+(check-expect (is-prime? 8) false)
+(check-expect (is-prime? 9) false)
+(check-expect (is-prime? 10) false)
+(check-expect (is-prime? 29) false)
+(check-expect (is-prime? 454) false)
 
-(define (is-prime? y x)
+
+(define (is-prime? x)
+  (is-prime-helper? x 2)
+  )
+
+
+(define (is-prime-helper? x y)
   (cond
     [(= x 1) false]
     [(= x 2) true]
     [(= 0 (modulo x y)) false]
     [(> y (sqrt x)) true]
-    [else (is-prime? (+ y 1) x)]
+    [else (is-prime-helper? (+ y 1) x)]
     )
   )
 
      
-;(define (evenly-divisible? 
+;(check-expect (largest-prime-factor 13195) 456)
+;
+;
 ;(define (largest-prime-factor x)
-;  5
+;  (largest-prime-factor-helper x 2)
 ; )
+;
+;(define (largest-prime-factor-helper x y)
+;  (cond
+;    [(> y (/ x 2)) x]
+;    [(= 0 (modulo x y))
+;        (cond
+;          [(> y (largest-prime-factor-helper x (+ 1 y))) y]
+;          [else (largest-prime-factor-helper x (+ 1 y))]
+;          )
+;        ]
+;    [else (largest-prime-factor-helper x (+ 1 y))]
+;    )
+;  )
